f2d30c241410f64f44a4120118480bb1b8b03ea3
volatilecat <kostiantyn.skorokhodov@hibob.io>
2024-12-06 16:43:45 +0100

    day5 try simd

--- src/day5.rs
+++ src/day5.rs
@@ -1,4 +1,11 @@
+use std::simd::prelude::*;
+
 pub fn part1(input: &str) -> u32 {
+    unsafe { part1_impl(input) }
+}
+
+#[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
+unsafe fn part1_impl(input: &str) -> u32 {
     let mut b = input.as_bytes();
     let mut table = [0u128; 100];
 
@@ -18,37 +25,104 @@ pub fn part1(input: &str) -> u32 {
 
     let mut sum = 0;
 
-    while let Some(eol) = memchr::memchr(b'\n', b) {
-        if let Some(page) = get_correct(&b[..eol], &table) {
+    let mut updates = [0u8; 64];
+
+    while let Some(eol) = find_newline(b) {
+        let count = parse_updates(&b[..eol], &mut updates);
+
+        if let Some(page) = get_correct(&updates[..count], &table) {
             sum += page as u32;
         }
+
         b = &b[(eol + 1)..];
     }
 
     sum
 }
 
+fn find_newline(mut b: &[u8]) -> Option<usize> {
+    let check = u8x32::splat(b'\n');
+    let mut pos: usize = 0;
+    loop {
+        if b.len() < 32 {
+            for i in 0..b.len() {
+                if b[i] == b'\n' {
+                    return Some(pos + i);
+                }
+            }
+            return None;
+        }
+        let a = u8x32::from_slice(&b[..32]);
+        if let Some(index) = a.simd_eq(check).first_set() {
+            return Some(pos + index);
+        }
+        b = &b[32..];
+        pos += 32;
+    }
+}
+
+fn parse_updates(mut b: &[u8], updates: &mut [u8; 64]) -> usize {
+    let ascii0 = u8x16::splat(b'0');
+    let ten = u8x16::splat(10);
+    let mut count = 0;
+
+    while !b.is_empty() {
+        let (a, len) = if b.len() < 32 {
+            (u8x32::load_or_default(&b[..]), b.len())
+        } else {
+            (u8x32::from_slice(&b[..32]), 32)
+        };
+
+        let a_hi: u8x16 = simd_swizzle!(
+            a,
+            [
+                0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, //
+                0, 0, 0, 0, 0, //
+            ]
+        );
+
+        let a_lo: u8x16 = simd_swizzle!(
+            a,
+            [
+                1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, //
+                0, 0, 0, 0, 0, //
+            ]
+        );
+
+        let r = (a_hi - ascii0) * ten + (a_lo - ascii0);
+        r.copy_to_slice(&mut updates[count..]);
+
+        let shift = (b.len() > 32) as usize;
+        b = &b[(len + shift)..];
+        count += (len + 1) / 3;
+    }
+
+    count
+}
+
 #[inline]
-fn get_correct(l: &[u8], table: &[u128; 100]) -> Option<u8> {
+fn get_correct(updates: &[u8], table: &[u128; 100]) -> Option<u8> {
     let mut printed = 0u128;
     let mut forbidden = 0u128;
 
-    for i in 0..((l.len() + 1) / 3) {
-        let page = (l[i * 3] - b'0') * 10 + (l[i * 3 + 1] - b'0');
-
+    for page in updates {
+        let page = *page as usize;
         if forbidden & (1 << page) != 0 {
             return None;
         }
-
-        forbidden |= table[page as usize] & !printed;
+        forbidden |= table[page] & !printed;
         printed |= 1 << page;
     }
 
-    let m = l.len() / 2 - 1;
-    Some((l[m] - b'0') * 10 + (l[m + 1] - b'0'))
+    Some(updates[updates.len() / 2])
 }
 
 pub fn part2(input: &str) -> u32 {
+    unsafe { part2_impl(input) }
+}
+
+#[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
+unsafe fn part2_impl(input: &str) -> u32 {
     let mut b = input.as_bytes();
     let mut table = [0u128; 100];
 
@@ -68,10 +142,15 @@ pub fn part2(input: &str) -> u32 {
 
     let mut sum = 0;
 
-    while let Some(eol) = memchr::memchr(b'\n', b) {
-        if let Some(page) = get_incorrect(&b[..eol], &table) {
+    let mut updates = [0u8; 64];
+
+    while let Some(eol) = find_newline(b) {
+        let count = parse_updates(&b[..eol], &mut updates);
+
+        if let Some(page) = get_incorrect(&updates[..count], &table) {
             sum += page as u32;
         }
+
         b = &b[(eol + 1)..];
     }
 
@@ -79,20 +158,19 @@ pub fn part2(input: &str) -> u32 {
 }
 
 #[inline]
-fn get_incorrect(l: &[u8], table: &[u128; 100]) -> Option<u8> {
-    let pages_len = (l.len() + 1) / 3;
+fn get_incorrect(updates: &[u8], table: &[u128; 100]) -> Option<u8> {
     let mut printed = 0u128;
     let mut forbidden = 0u128;
     let mut incorrect = false;
 
-    for i in 0..pages_len {
-        let page = (l[i * 3] - b'0') * 10 + (l[i * 3 + 1] - b'0');
+    for page in updates {
+        let page = *page as usize;
 
         if forbidden & (1 << page) != 0 {
             incorrect = true;
         }
 
-        forbidden |= table[page as usize] & !printed;
+        forbidden |= table[page] & !printed;
         printed |= 1 << page;
     }
 
@@ -100,12 +178,12 @@ fn get_incorrect(l: &[u8], table: &[u128; 100]) -> Option<u8> {
         return None;
     }
 
-    for i in 0..pages_len {
-        let page = ((l[i * 3] - b'0') * 10 + (l[i * 3 + 1] - b'0')) as usize;
+    for page in updates {
+        let page = *page as usize;
 
         let req_cnt = (table[page] & printed).count_ones() as usize;
 
-        if req_cnt == pages_len / 2 {
+        if req_cnt == updates.len() / 2 {
             return Some(page as u8);
         }
     }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(portable_simd)]
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
